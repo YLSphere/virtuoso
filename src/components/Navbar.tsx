b116e88becbd23d0bd5e7f8b069ee4833b53ef9d
YLSphere <lai.myron@gmail.com>
2024-05-27 12:28:01 -0700

    working UI no genre

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/Navbar.css';
-import { useNavigate } from "react-router-dom";
-import { FaSpotify } from "react-icons/fa";
+import { MdOutlineLogout } from "react-icons/md";
+import { TbFilter } from "react-icons/tb";
 import axios from 'axios';
 
 interface UserProfile {
